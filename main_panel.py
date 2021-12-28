854171df129224ce9e4fd1e9d6ae154c52811c96
NiteShad0w <matey.brezovic@gmail.com>
2021-12-28 21:48:29 +0100

    started working on main panel

--- main_panel.py
+++ main_panel.py
@@ -1,4 +1,7 @@
 from PyQt6 import QtWidgets
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QComboBox, QVBoxLayout
+
 from constants import *
 from tag_manager import TagManager
 
@@ -7,7 +10,26 @@ class MainPanel(QtWidgets.QFrame):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.setStyleSheet("background-color: rgba(255, 255, 0, 0.3)")
+        self.setStyleSheet("MainPanel {background-color: rgba(255, 255, 0, 0.3)}")
         self.setMinimumWidth(MAIN_PANEL_MIN_WIDTH)
         self.tag_manager = TagManager()
 
+        self.view_options = {
+            0: "Tracks",
+            1: "Album and Tracks",
+            2: "Album Covers",
+            3: "Artists",
+        }
+
+        self.view_options_combo_box = QComboBox()
+        self.view_options_combo_box.setFixedWidth(150)
+        self.view_options_combo_box.currentIndexChanged.connect(self.view_key_changed)
+        self.view_options_combo_box.addItems(self.view_options.values())
+
+        self.main_layout = QVBoxLayout(self)
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.main_layout.addWidget(self.view_options_combo_box)
+
+    def view_key_changed(self):
+        ...
--- navigation_panel.py
+++ navigation_panel.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from typing import List
 
-from PyQt6 import QtWidgets
+from PyQt6 import QtWidgets, QtGui
 from PyQt6.QtWidgets import QWidget, QFrame, QScrollArea
 
 from constants import *
@@ -35,7 +35,6 @@ class NavigationPanel(QtWidgets.QFrame):
         self.group_container_layout.setContentsMargins(0, 0, 0, 0)
         self.group_container_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
         self.group_combo_box = QtWidgets.QComboBox()
-        self.group_combo_box_index = 0
         self.group_combo_box.currentIndexChanged.connect(self.group_key_changed)
         self.group_combo_box.addItems(self.group_options.values())
 
@@ -47,6 +46,7 @@ class NavigationPanel(QtWidgets.QFrame):
 
     def _load_groups(self, key: int = 0):
         self.groups = defaultdict(lambda: [])
+        self.group_widgets = []
 
         for track in TRACKS:
             if self.group_options[key].lower() != "folder":
@@ -61,14 +61,17 @@ class NavigationPanel(QtWidgets.QFrame):
         self.groups = {x: self.groups[x] for x in sorted(self.groups)}
         for group in self.groups:
             title = group
-            subtitle = str(str(len(self.groups[group])) + " " + ("Tracks" if self.group_combo_box_index == 0
+            subtitle = str(str(len(self.groups[group])) + " " + ("Tracks" if self.group_combo_box.currentIndex() == 0
                                                                  else "Tracks"))[:(-1 if len(self.groups[group])
                                                                                    == 1 else 10)]
             group_widget = GroupWidget(title, subtitle, self.group_options[key], self.groups[group])
+            self.group_widgets.append(group_widget)
             self.group_container_layout.addWidget(group_widget)
 
+        for group_widget in self.group_widgets:
+            group_widget.group_widgets = self.group_widgets
+
     def group_key_changed(self, new_key: int):
-        self.group_combo_box_index = new_key
         delete_items(self.group_container_layout)
         self._load_groups(new_key)
 
@@ -76,13 +79,16 @@ class NavigationPanel(QtWidgets.QFrame):
 class GroupWidget(QFrame):
     def __init__(self, title: str, subtitle: str, group_type: str, tracks: List[str]):
         super().__init__()
-        self.setStyleSheet("GroupWidget {background-color: rgba(18, 178, 255, 0.3)}")
+        self.default_stylesheet = "GroupWidget {background-color: rgba(18, 178, 255, 0.3)}"
+        self.selected_stylesheet = "GroupWidget {background-color: rgba(0, 0, 0, 0.3)}"
+        self.setStyleSheet(self.default_stylesheet)
         self.setContentsMargins(0, 0, 0, 0)
         self.setFixedHeight(60)
         self.title = title
         self.subtitle = subtitle
         self.tracks = tracks
         self.tag_manager = TagManager()
+        self.group_widgets = []
 
         self.title_label = ElidedLabel(self.title)
 
@@ -91,16 +97,17 @@ class GroupWidget(QFrame):
         self.image_label = QLabel()
         self.image_label.setFixedSize(60, 60)
 
-        pixmap = get_artwork_pixmap(self.tracks[0], group_type)
-        self.image_label.setPixmap(pixmap.scaled(self.image_label.width() - 4,
-                                                 self.image_label.height() - 4,
-                                                 Qt.AspectRatioMode.KeepAspectRatio,
-                                                 Qt.TransformationMode.SmoothTransformation))
+        self.artwork_pixmap = get_artwork_pixmap(self.tracks[0], group_type)
+        self.image_label.setPixmap(self.artwork_pixmap.scaled(self.image_label.width() - 4,
+                                                              self.image_label.height() - 4,
+                                                              Qt.AspectRatioMode.KeepAspectRatio,
+                                                              Qt.TransformationMode.SmoothTransformation))
 
         self.text_widget = QWidget()
 
         self.vertical_layout = QtWidgets.QVBoxLayout(self.text_widget)
         self.vertical_layout.setContentsMargins(0, 0, 0, 0)
+        self.vertical_layout.setSpacing(0)
         self.vertical_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
         self.vertical_layout.addWidget(self.title_label)
         self.vertical_layout.addWidget(self.subtitle_label)
@@ -109,3 +116,8 @@ class GroupWidget(QFrame):
         self.horizontal_layout.setContentsMargins(0, 0, 0, 0)
         self.horizontal_layout.addWidget(self.image_label)
         self.horizontal_layout.addWidget(self.text_widget)
+
+    def mousePressEvent(self, event: QtGui.QMouseEvent) -> None:
+        for group_widget in self.group_widgets:
+            group_widget.setStyleSheet(self.default_stylesheet)
+        self.setStyleSheet(self.selected_stylesheet)
