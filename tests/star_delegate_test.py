22a52458c735ac6d77cf41a373ab8a68da7d7cf0
NiteShad0w <matey.brezovic@gmail.com>
2022-04-07 22:31:43 +0200

    working on star delegate

--- tests/star_delegate_test.py
+++ tests/star_delegate_test.py
@@ -1,32 +1,93 @@
 import math
 
 from PyQt6.QtCore import pyqtSignal, QPointF, QSize, Qt
+from PyQt6.QtGui import QColor
 from PyQt6.QtGui import QPainter, QPolygonF, QMoveEvent
 from PyQt6.QtWidgets import (QAbstractItemView, QApplication, QStyle,
                              QStyledItemDelegate, QTableWidget, QTableWidgetItem, QWidget)
-from PyQt6.QtGui import QColor
+
+
+class StarPolygon(QPolygonF):
+    def __init__(self, size: float):
+        super().__init__()
+
+        # offset_x = -0
+        # offset_y = 100
+
+        for i in range(5):
+            x = math.cos(0.8 * i * math.pi - math.pi / 2)
+            y = math.sin(0.8 * i * math.pi - math.pi / 2)
+            self << QPointF(size * x, size *  y)
+
+
+class HalfStarPolygon(QPolygonF):
+    def __init__(self, size: float):
+        super().__init__()
+
+        # star_polygon_size = 40
+        half_star_polygon_size = size / 2
+        star_angle_step = 2 * math.pi / 5
+        star_angle_start = - math.pi / 2
+        half_star_angle_start = star_angle_start - star_angle_step / 2
+
+        x = math.cos(star_angle_start)
+        y = math.sin(star_angle_start)
+        self << QPointF(size * x, size * y)
+
+        x = math.cos(half_star_angle_start)
+        y = math.sin(half_star_angle_start)
+        self << QPointF(half_star_polygon_size * x, half_star_polygon_size * y)
+
+        star_angle_start -= star_angle_step
+        x = math.cos(star_angle_start)
+        y = math.sin(star_angle_start)
+        self << QPointF(size * x, size * y)
+
+        half_star_angle_start -= star_angle_step
+        x = math.cos(half_star_angle_start)
+        y = math.sin(half_star_angle_start)
+        self << QPointF(half_star_polygon_size * x, half_star_polygon_size * y)
+
+        star_angle_start -= star_angle_step
+        x = math.cos(star_angle_start)
+        y = math.sin(star_angle_start)
+        self << QPointF(size * x, size * y)
+
+        half_star_angle_start -= star_angle_step
+        x = math.cos(half_star_angle_start)
+        y = math.sin(half_star_angle_start)
+        self << QPointF(half_star_polygon_size * x, half_star_polygon_size * y)
+
+
+def get_star_polygon():
+    star_polygon = QPolygonF()
+
+    for i in range(5):
+        star_polygon_size = 0.5
+        default_x = math.cos(0.8 * i * math.pi - math.pi / 2)
+        default_y = math.sin(0.8 * i * math.pi - math.pi / 2)
+        star_polygon << QPointF(1 + star_polygon_size * default_x,
+                                1 + star_polygon_size * default_y)
+    return star_polygon
 
 
 class StarRating:
     # enum EditMode
     Editable, ReadOnly = False, True
-    PaintingScaleFactor = 20
+    # PaintingScaleFactor = 10
 
-    def __init__(self, star_count=1, max_star_count=5):
-        self._star_count = star_count
-        self._max_star_count = max_star_count
+    def __init__(self, star_count: float = 0, max_star_count: float = 5):
+        self._star_count = float(star_count)
+        self._max_star_count = float(max_star_count)
 
-        self.star_polygon = QPolygonF([QPointF(1.0, 0.5)])
-        for i in range(5):
-            self.star_polygon << QPointF(0.5 + 0.5 * math.cos(0.8 * i * math.pi),
-                                        0.5 + 0.5 * math.sin(0.8 * i * math.pi))
+        self.star_polygon_size = 10
+        half_star_polygon_size = 0.3
+        star_angle_step = 2 * math.pi / 5
+        star_angle_start = - math.pi / 2
+        half_star_angle_start = star_angle_start + star_angle_step / 2
 
-        self.diamond_polygon = QPolygonF()
-        self.diamond_polygon << QPointF(0.4, 0.5) \
-                             << QPointF(0.5, 0.4) \
-                             << QPointF(0.6, 0.5) \
-                             << QPointF(0.5, 0.6) \
-                             << QPointF(0.4, 0.5)
+        self.star_polygon = StarPolygon(self.star_polygon_size)
+        self.half_star_polygon = HalfStarPolygon(self.star_polygon_size)
 
     def star_count(self):
         return self._star_count
@@ -34,14 +95,15 @@ class StarRating:
     def max_star_count(self):
         return self._max_star_count
 
-    def set_star_count(self, star_count):
+    def set_star_count(self, star_count: float):
+        # print(star_count)
         self._star_count = star_count
 
-    def set_max_star_count(self, max_star_count):
+    def set_max_star_count(self, max_star_count: float):
         self._max_star_count = max_star_count
 
     def size_hint(self):
-        return self.PaintingScaleFactor * QSize(self._max_star_count, 1)
+        return self.star_polygon_size * QSize(self._max_star_count, 1) * 2
 
     def paint(self, painter, rect, palette, edit_mode):
         painter.save()
@@ -54,18 +116,15 @@ class StarRating:
         else:
             painter.setBrush(palette.windowText())
 
-        y_offset = (rect.height() - self.PaintingScaleFactor) / 2
-        painter.translate(rect.x(), rect.y() + y_offset)
-        painter.scale(self.PaintingScaleFactor, self.PaintingScaleFactor)
+        y_offset = (rect.height() - self.star_polygon_size) / 2
+        painter.translate(rect.x() + self.star_polygon_size, rect.y() + y_offset)
 
-        for i in range(self._max_star_count):
-            if i < self._star_count:
+        for i in range(int(self._star_count * 2)):
+            if not i % 2:
+                painter.drawPolygon(self.half_star_polygon, Qt.FillRule.WindingFill)
+            else:
                 painter.drawPolygon(self.star_polygon, Qt.FillRule.WindingFill)
-            elif edit_mode == StarRating.Editable:
-                painter.setBrush(QColor("red"))
-                painter.drawPolygon(self.diamond_polygon, Qt.FillRule.WindingFill)
-
-            painter.translate(1.0, 0.0)
+                painter.translate(self.star_polygon_size * 2, 0.0)
 
         painter.restore()
 
@@ -76,41 +135,46 @@ class StarEditor(QWidget):
     def __init__(self, parent = None):
         super().__init__(parent)
 
-        self._starRating = StarRating()
+        self._star_rating = StarRating()
 
         self.setMouseTracking(True)
         self.setAutoFillBackground(True)
+        # self.setStyleSheet("background-color: red")
 
-    def set_star_rating(self, star_rating):
-        self._starRating = star_rating
+    def set_star_rating(self, star_rating: float):
+        self._star_rating = star_rating
 
     def star_rating(self):
-        return self._starRating
+        return self._star_rating
 
     def sizeHint(self):
-        return self._starRating.size_hint()
+        return self._star_rating.size_hint()
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        self._starRating.paint(painter, self.rect(), self.palette(),
+        self._star_rating.paint(painter, self.rect(), self.palette(),
                 StarRating.Editable)
 
     def mouseMoveEvent(self, event: QMoveEvent):
+        print(event.pos().x())
         star = self.star_at_position(event.pos().x())
 
-        if star != self._starRating.star_count() and star != -1:
-            self._starRating.set_star_count(star)
+        if star != self._star_rating.star_count() and star != -1:
+            self._star_rating.set_star_count(star)
             self.update()
 
     def mouseReleaseEvent(self, event):
         self.editing_finished.emit()
 
     def star_at_position(self, x):
+        if not x:
+            return 0
+
         # Enable a star, if pointer crosses the center horizontally.
-        star_width = self._starRating.size_hint().width() // self._starRating.max_star_count()
-        star = (x + star_width / 2) // star_width
-        if 0 <= star <= self._starRating.max_star_count():
-            return star
+        star_width = self._star_rating.size_hint().width() // self._star_rating.max_star_count()
+        num_of_stars = (x + star_width / 2) / star_width // 0.5 / 2
+        if 0 <= num_of_stars <= self._star_rating.max_star_count():
+            return num_of_stars
 
         return -1
 
@@ -122,8 +186,7 @@ class StarDelegate(QStyledItemDelegate):
             if option.state & QStyle.StateFlag.State_Selected:
                 painter.fillRect(option.rect, option.palette.highlight())
 
-            star_rating.paint(painter, option.rect, option.palette,
-                    StarRating.ReadOnly)
+            star_rating.paint(painter, option.rect, option.palette, StarRating.ReadOnly)
         else:
             super().paint(painter, option, index)
 
---
+++ tests/star_test.py
@@ -0,0 +1,129 @@
+import math
+import sys
+
+from PyQt6.QtCore import QPointF, Qt
+from PyQt6.QtGui import QPolygonF, QPainter, QPen
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.resize(600, 600)
+        self.show()
+
+    @staticmethod
+    def get_star_polygon():
+        star_polygon = QPolygonF()
+        star_polygon_size = 10
+
+        for i in range(5):
+            default_x = math.cos(0.8 * i * math.pi - math.pi / 2)
+            default_y = math.sin(0.8 * i * math.pi - math.pi / 2)
+            star_polygon << QPointF(100 + star_polygon_size * default_x,
+                                    100 + star_polygon_size * default_y)
+        return star_polygon
+
+    @staticmethod
+    def get_quarter_polygon():
+        offset_x = 100
+        offset_y = 100
+        star_polygon_size = 40
+
+        star_polygon = QPolygonF()
+        star_polygon << QPointF(offset_x, offset_y)
+        for i in range(10):
+            default_x = math.cos(math.radians(i))
+            default_y = math.sin(math.radians(i))
+            star_polygon << QPointF(offset_x + star_polygon_size * default_x,
+                                    offset_y + star_polygon_size * default_y)
+        return star_polygon
+
+    def get_x_axis(self):
+        offset_x = 100
+        offset_y = 100
+
+        star_polygon = QPolygonF()
+        star_polygon << QPointF(0, offset_y) <<  QPointF(600, offset_y)
+
+        return star_polygon
+
+    def get_y_axis(self):
+        offset_x = 100
+        offset_y = 100
+
+        star_polygon = QPolygonF()
+        star_polygon << QPointF(offset_x, 0) << QPointF(offset_x, 600)
+
+        return star_polygon
+
+    def get_half_star_polygon(self):
+        star_polygon_size = 40
+        half_star_polygon_size = 20
+        star_angle_step = 2 * math.pi / 5
+        star_angle_start = - math.pi / 2
+        half_star_angle_start = star_angle_start - star_angle_step / 2
+
+        offset_x = 100
+        offset_y = 100
+
+        self.star_polygon = QPolygonF()
+
+        x = math.cos(star_angle_start)
+        y = math.sin(star_angle_start)
+        self.star_polygon << QPointF(offset_x + star_polygon_size * x, offset_y + star_polygon_size * y)
+
+        x = math.cos(half_star_angle_start)
+        y = math.sin(half_star_angle_start)
+        self.star_polygon << QPointF(offset_x + half_star_polygon_size * x, offset_y + half_star_polygon_size * y)
+
+        star_angle_start -= star_angle_step
+        x = math.cos(star_angle_start)
+        y = math.sin(star_angle_start)
+        self.star_polygon << QPointF(offset_x + star_polygon_size * x, offset_y + star_polygon_size * y)
+        #
+        half_star_angle_start -= star_angle_step
+        x = math.cos(half_star_angle_start)
+        y = math.sin(half_star_angle_start)
+        self.star_polygon << QPointF(offset_x + half_star_polygon_size * x, offset_y + half_star_polygon_size * y)
+        #
+        star_angle_start -= star_angle_step
+        x = math.cos(star_angle_start)
+        y = math.sin(star_angle_start)
+        self.star_polygon << QPointF(offset_x + star_polygon_size * x, offset_y + star_polygon_size * y)
+
+        half_star_angle_start -= star_angle_step
+        x = math.cos(half_star_angle_start)
+        y = math.sin(half_star_angle_start)
+        self.star_polygon << QPointF(offset_x + half_star_polygon_size * x, offset_y + half_star_polygon_size * y)
+
+        return self.star_polygon
+
+    def draw_polygon(self, polygon, line_color):
+        painter = QPainter(self)
+        painter.setPen(QPen(line_color, 1, Qt.PenStyle.SolidLine))
+        painter.drawPolygon(polygon)
+
+    def paintEvent(self, event):
+        self.draw_polygon(self.get_x_axis(), Qt.GlobalColor.red)
+        self.draw_polygon(self.get_y_axis(), Qt.GlobalColor.red)
+        self.draw_polygon(self.get_star_polygon(), Qt.GlobalColor.black)
+
+
+        # painter = QPainter(self)
+        # painter.setPen(QPen(Qt.GlobalColor.black, 5, Qt.PenStyle.SolidLine))
+        # painter.setBrush(QBrush(Qt.red, Qt.SolidPattern))
+        # painter.setBrush(QBrush(Qt.GlobalColor.black, Qt.BrushStyle.SolidPattern))
+
+        # polygon = self.get_half_star_polygon()
+        # # polygon = self.get_quarter_polygon()
+        #
+        # painter.drawPolygon(polygon)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec())
