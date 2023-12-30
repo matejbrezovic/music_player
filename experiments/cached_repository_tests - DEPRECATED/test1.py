e216fd69f12b546444aebc90f9346e4d7be3317e
Matej Brezovic <matej.brezovic64@gmail.com>
2023-12-30 21:04:09 +0100

    excluded experiments dir

--- experiments/cached_repository_tests - DEPRECATED/test1.py
+++
@@ -1,9 +0,0 @@
-from repositories.cached_tracks_repository import CachedTracksRepository
-
-
-def test_func1():
-    print("Test 1 start")
-    cr = CachedTracksRepository()
-    cr.load_cache()
-    print(cr.get_tracks_by("album", None))
-    print("Test 1 end")
